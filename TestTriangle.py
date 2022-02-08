6524e15572d1538e2014995b8aa4cdcaa6cb9a31
Amit <abhatna7@stevens.edu>
2022-02-08 18:59:48 -0500

    TestTriangle.py added set of test cases

--- TestTriangle.py
+++ TestTriangle.py
@@ -25,7 +25,35 @@ class TestTriangles(unittest.TestCase):
         
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
-
+        
+    # test case: isosceles
+    def testIsosceles(self):
+        self.assertEqual(classifyTriangle(5,5,8), 'Isosceles', '5,5,8 is an Isosceles triangle')
+        
+    # test case: scalene
+    def testScalene(self):
+        self.assertEqual(classifyTriangle(7,4,2), 'Scalene', '7,4,2 is a Scalene triangle')
+    
+    # test case: is a triangle
+    def testValidTriangleA(self):
+        self.assertEqual(classifyTriangle(4,1,8), 'NotATriangle', '4,1,8 is not a valid triangle')
+    
+    # test case: is a triangle when c is not the largest number
+    def testValidTriangleB(self):
+        self.assertNotEquals(classifyTriangle(8,3,6), 'NotATriangle', '4,1,8 is not a valid triangle')
+    
+    # test case: valid input over 200
+    def testValidInputsA(self):
+        self.assertEquals(classifyTriangle(250, 10, 199), 'InvalidInput', 'One or more inputs is out of bounds (over 200)')
+    
+    # test valid inputs under 0
+    def testValidInputsB(self):
+        self.assertEquals(classifyTriangle(3, -5, 2), 'InvalidInput', 'One or more inputs is out of bounds (negative)')
+    
+    # test case: triangle is given non-integer values
+    def testNonInts(self):
+        self.assertEquals(classifyTriangle(4.6, 3.2, 5.1), 'InvalidInput', 'Non-integer values given')
+        
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
